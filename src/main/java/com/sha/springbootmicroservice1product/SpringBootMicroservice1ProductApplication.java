cb31c5bb592bce77ecdfd4190b3ffd8a336572d4
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-09 21:22:45 +0300

    project created

---
+++ src/main/java/com/sha/springbootmicroservice1product/SpringBootMicroservice1ProductApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootmicroservice1product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice1ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice1ProductApplication.class, args);
+	}
+
+}
