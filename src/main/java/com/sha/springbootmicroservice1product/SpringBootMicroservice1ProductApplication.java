4e496d639bf0cb04a67d66e67a98040fef8f1b16
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-10 13:22:03 +0300

    configuration profiles

--- src/main/java/com/sha/springbootmicroservice1product/SpringBootMicroservice1ProductApplication.java
+++ src/main/java/com/sha/springbootmicroservice1product/SpringBootMicroservice1ProductApplication.java
@@ -2,8 +2,10 @@ package com.sha.springbootmicroservice1product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroservice1ProductApplication {
 
     public static void main(String[] args) {
