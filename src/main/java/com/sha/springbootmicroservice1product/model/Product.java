7cc12c45fe5a74ebca2354aeda174523140e3432
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-10 10:22:13 +0300

    product entity

---
+++ src/main/java/com/sha/springbootmicroservice1product/model/Product.java
@@ -0,0 +1,25 @@
+package com.sha.springbootmicroservice1product.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+}
